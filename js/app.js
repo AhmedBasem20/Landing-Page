ad498c67ac14435c4209779f91088c1aa4ec5af3
Ahmed Basem <62952819+AhmedBasem20@users.noreply.github.com>
2021-12-15 04:33:52 +0200

    Update app.js
    Add highlighted() and onScroll() functions

--- js/app.js
+++ js/app.js
@@ -1,50 +1,110 @@
-/**
- * 
- * Manipulating the DOM exercise.  
- * Exercise programmatically builds navigation,   1
- * scrolls to anchors from navigation,   2
- * and highlights section in viewport upon scrolling.   3
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
-*/
 const navBar = document.querySelector('#navbar__list');
-const navSec = document.querySelectorAll('section');
+const section = document.querySelectorAll('section');
 
 
 // build the nav
 function buildNavBar() {
 
     const createFragment = document.createDocumentFragment();
-    for (let sec of navSec) {
-        const navli = document.createElement('li');
+    for (let sec of section) {
+        const li = document.createElement('li');
         const nava = document.createElement('a');
         nava.textContent = sec.getAttribute('data-nav');
         nava.setAttribute('class', 'menu__link');
 
         // Scroll to anchor ID
         nava.addEventListener("click", () => {
-            sec.scrollIntoView({behavior: "smooth"});
+            sec.scrollIntoView({behavior: "smooth", block:'center'});
+            for (let sec of section) {
+                sec.classList.remove('your-active-class');
+            }
+            sec.classList.add('your-active-class');
             });
-        navli.appendChild(nava);
-        createFragment.appendChild(navli);
+       
+                // const limit = sec.getBoundingClientRect().top;
+                // if (limit > 0 && limit < 350) {
+                //         for (let sec of section) {
+                //             sec.classList.remove('your-active-class');
+                //         }
+                //         sec.classList.add('your-active-class');
+                //         const navlink = document.querySelector(`[href="#${sec.id}"]`);
+                //         nava.style.color='green';
+
+                //     }
+
+                
+
+
+        li.appendChild(nava);
+        createFragment.appendChild(li);
+        
+
     };
     navBar.appendChild(createFragment);
+    
+
+
+
+
+};
+
+
+function highlighted() {
+    let atag = navBar.getElementsByClassName("menu__link");
+    for (let i = 0; i < atag.length; i++) {
+        atag[i].addEventListener("click", function(){
+            let current = document.getElementsByClassName("highlight");
+
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" highlight", "");
+    }
+
+    // Add the active class to the current/clicked button
+    this.className += " highlight";
+        });
+    }
+};
+
+
+
+function onScroll() {
+    window.addEventListener('scroll', function(){
+        let current = "";
+        for (sec of section) {
+            let sectionTop = sec.offsetTop;
+            let sectioHeigt = sec.clientHeight;
+            let atag = navBar.getElementsByClassName("menu__link");
+            if (window.pageYOffset > sectionTop - sectioHeigt / 3) {
+                current = sec.getAttribute('data-nav');
+                for (let i = 0; i < atag.length; i++)
+                {
+
+                    if (atag[i].textContent === current)
+                    {
+                        atag[i].classList.add("highlight");
+                        //remove highlight class from unviewed sections
+                        for (let j = 0; j < atag.length; j++)
+                        {
+                            if (atag[j] != atag[i])
+                            {
+                                atag[j].classList.remove("highlight");
+                            }
+                        }
+
+                    }
+                }
+
+            }
+        }
+        console.log(current);
+        
+        
+
+    });
 };
 
 // Build navigation menu
 buildNavBar();
-
-// Set sections as active (highlight section and nav if section is in viewport)
-/**
- * To Do...
- * highlights section in viewport upon scrolling.   3
- */
+highlighted();
+onScroll();
